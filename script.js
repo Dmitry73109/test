ea2526f8709f3afc0477ff8ce4388b019bdef96d
Dmitry73109 <dmitry73109@gmail.com>
2025-04-10 16:31:32 +0300

    it was dffclt

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-})
+    $("#flipToLogin").on("click", function(e) {
+      e.preventDefault();
+      $("#flipContainer").addClass("flipped");
+    });
+  
+    $("#flipToRegister").on("click", function(e) {
+      e.preventDefault();
+      $("#flipContainer").removeClass("flipped");
+    });
+  
+    $("#loginButton").on('click', function(e) {
+        e.preventDefault();
+      
+        let password = $("#login-password").val();
+        let email = $("#login-email").val();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      
+        if (password.length >= 8 && emailRegex.test(email)) {
+          Swal.fire("Done!", "U did it!", "success");
+        } else {
+          Swal.fire("Hmmm, no", "password|email wrong!", "error");
+        }
+      });
+      
+  
+    $("#submitButton").on('click', function(e) {
+      e.preventDefault();
+  
+      let password = $("#password").val();
+      let confirmPassword = $("#confirm-password").val();
+      let email = $("#email").val();
+      let hasSymbol = /[!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|-]/.test(password);
+      let name = $("#username").val();
 
-$("#submitButton").on('click', function(e){
-e.preventDefault();
+      if(/[0-9]/.test(name)){
+        Swal.fire("Hmmm, no", "name wrong!", "error");
+        return;
+      }else if(/[!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|-]/.test(name)){
+        Swal.fire("Hmmm, no", "name wrong!", "error");
+        return;
+      }
+  
+      if (password !== confirmPassword) {
+        Swal.fire("Hmmm, no", "Passwords do not match!", "error");
+      } else if (hasSymbol && password.length >= 8 && email.includes("@")) {
+        Swal.fire("Done!", "U did it!", "success");
+      } else {
+        Swal.fire("Hmmm, no", "password|name wrong!", "error");
+      }
+    });
 
-let password = $("#password").val();
-let email = $("#email").val();
-let hasSymbol = /[!@#$%^&*()_+{}\[\]:;"'<>,.?\\\/|-]/.test(password);
-
-if(hasSymbol && password.length>=8 && email.includes("@")){
-alert("ok");
-}else{
-alert("not ok");
-}
-
-})
-
+    $("#showPasswords").on("change", function () {
+        const type = $(this).is(":checked") ? "text" : "password";
+        $("#password, #confirm-password").attr("type", type);
+      });
+    
+      $("#showLoginPassword").on("change", function () {
+        const type = $(this).is(":checked") ? "text" : "password";
+        $("#login-password").attr("type", type);
+      });
+      
+  
+  });
+  
